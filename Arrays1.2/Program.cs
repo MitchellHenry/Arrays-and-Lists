f63be370e0e38464701792e35cbfadaa72f94b3d
MitchellHenry <48079181+MitchellHenry@users.noreply.github.com>
2019-03-27 23:43:40 +1100

    Added FIles

---
+++ Arrays1.2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mitchell
+ * Date: 27/03/2019
+ * Time: 11:29 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Arrays1._
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        int[] numbers = {34, 5, 67, 1, 99, 34, 44, 78, 34, 0};
+         
+        int count = 0;
+        int total = 0;
+        
+        while( count < numbers.Length){
+        	total = total + numbers[count];
+        	count++;
+             
+        }
+        Console.WriteLine("the Total is " + total);
+       
+        
+
+        Console.ReadKey(true);
+    }
+    }
+}
---
+++ Lists1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mitchell
+ * Date: 27/03/2019
+ * Time: 11:30 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace LIsts1._
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
