6f93042066ebcda0fc185b65c1a1255ddbfd6851
MitchellHenry <48079181+MitchellHenry@users.noreply.github.com>
2019-04-04 16:28:24 +1100

    Added Files

---
+++ Lists1.1/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mitchell
+ * Date: 4/04/2019
+ * Time: 3:10 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Lists1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+	        
+			Console.WriteLine("Give me five numbers");
+			
+			var values = new List<int>();
+			
+			values.Add(int.Parse(Console.ReadLine()));
+			values.Add(int.Parse(Console.ReadLine()));
+			values.Add(int.Parse(Console.ReadLine()));
+			values.Add(int.Parse(Console.ReadLine()));
+			values.Add(int.Parse(Console.ReadLine()));
+			
+			int count = 0;
+			
+			Console.WriteLine("Give me one more number");
+			
+			while(count <= 5){
+				if(int.Parse(Console.ReadLine()) == values[count]){
+				 
+					Console.WriteLine("That number has already been entered!");
+					count = 10;
+				}
+				count++;
+			
+			}
+			
+			
+			Console.ReadKey(true);
+		}
+	}
+}
